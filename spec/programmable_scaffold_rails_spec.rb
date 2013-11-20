661da6ef2b55371561d9d2c0b34e979805c1f8b4
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-11-20 04:59:49 +0100

    Improved readability on tests for BASE_OPTIONS and CRUD_METHODS

--- spec/programmable_scaffold_rails_spec.rb
+++ spec/programmable_scaffold_rails_spec.rb
@@ -4,22 +4,24 @@ require 'programmable_scaffold_rails/config'
 
 describe ProgrammableScaffoldRails do
   subject(:base_options) { described_class::BASE_OPTIONS }
+    specify { base_options.should_not be_nil                                }
+    specify { base_options.should have_key(:cancan)                         }
+    specify { base_options.should have_key(:friendly_id)                    }
+    specify { base_options.should have_key(:strong_params)                  }
+    specify { base_options.should have_key(:formats)                        }
+    specify { base_options[:cancan].should be_true                          }
+    specify { base_options[:friendly_id].should be_false                    }
+    specify { base_options[:strong_params].should be_an_instance_of(Symbol) }
+    specify { base_options[:formats].should match_array([:html, :json])     }
 
-  it { base_options.should_not be_nil                                }
-  it { base_options.should have_key(:cancan)                         }
-  it { base_options.should have_key(:friendly_id)                    }
-  it { base_options.should have_key(:strong_params)                  }
-  it { base_options[:cancan].should be_true                          }
-  it { base_options[:friendly_id].should be_false                    }
-  it { base_options[:strong_params].should be_an_instance_of(Symbol) }
-
-  it { described_class::CRUD_METHODS.should_not be_nil        }
-  it { described_class::CRUD_METHODS.should include(:new)     }
-  it { described_class::CRUD_METHODS.should include(:create)  }
-  it { described_class::CRUD_METHODS.should include(:index)   }
-  it { described_class::CRUD_METHODS.should include(:show)    }
-  it { described_class::CRUD_METHODS.should include(:edit)    }
-  it { described_class::CRUD_METHODS.should include(:update)  }
-  it { described_class::CRUD_METHODS.should include(:destroy) }
+  subject(:crud_methods) { described_class::CRUD_METHODS }
+    specify { crud_methods.should_not be_nil        }
+    specify { crud_methods.should include(:new)     }
+    specify { crud_methods.should include(:create)  }
+    specify { crud_methods.should include(:index)   }
+    specify { crud_methods.should include(:show)    }
+    specify { crud_methods.should include(:edit)    }
+    specify { crud_methods.should include(:update)  }
+    specify { crud_methods.should include(:destroy) }
 
 end
