65a83993d820ef564896dd1ee0f3f44246ac6752
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2013-11-25 06:13:28 +0100

    Added a TODO for other tests

--- spec/controllers/dummies_controller_spec.rb
+++ spec/controllers/dummies_controller_spec.rb
@@ -60,6 +60,59 @@ describe DummiesController do
     expect{dummy.class.find(dummy.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 
+  context "with json" do
+
+    # TODO: Write tests to ensure formats html/json are not called if not enabled
+
+    it "POST #create" do
+      dummy_params = FactoryGirl.attributes_for(:dummy)
+      
+      expect{post :create, dummy: dummy_params, format: :json}.to change(Dummy, :count).by(1)
+    end
+
+    # it "GET #index" do
+    #   dummies = FactoryGirl.create_list(:dummy, 2)
+
+    #   get :index
+
+    #   assigns(:dummies).should match_array(dummies)
+    # end
+
+    # it "GET #show" do
+    #   dummy = FactoryGirl.create(:dummy)
+
+    #   get :show, id: dummy.id
+
+    #   assigns(:dummy).should == dummy
+    # end
+
+    # it "GET #edit" do
+    #   dummy = FactoryGirl.create(:dummy)
+
+    #   get :edit, id: dummy.id
+
+    #   assigns(:dummy).should == dummy
+    # end
+
+    # it "PUT/PATCH #update" do
+    #   dummy_name          = 'dummy2'
+    #   dummy               = FactoryGirl.create(:dummy, name: 'dummy1')
+
+    #   put :update, id: dummy.id, dummy: { name: dummy_name }
+
+    #   assigns(:dummy).name.should match dummy_name
+    # end
+
+    # it "DELETE #destroy" do
+    #   dummy = FactoryGirl.create(:dummy)
+      
+    #   delete :destroy, id: dummy.id
+
+    #   expect{dummy.class.find(dummy.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    # end
+
+  end
+
   context "without authorization" do
     before(:each) do
       controller.stub(:authorize!).with(anything(), anything()).and_raise
